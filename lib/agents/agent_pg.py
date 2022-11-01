dc0f908f72b6e90d6aac6cb895a86e50b6a6934b
kangyao huang <kangyao.huang@outlook.com>
2022-11-01 17:10:01 +0800

    upload

---
+++ lib/agents/agent_pg.py
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------------------------------------------------------- #
+#   @description: Class AgentPG
+#   @author: Modified from khrylib by Ye Yuan, modified by Kangyao Huang
+#   @created date: 01.Nov.2022
+# ------------------------------------------------------------------------------------------------------------------- #
+
+from lib.agents.agent import Agent
+
+
+class AgentPG(Agent):
+    def __init__(self, tau=0.95, optimizer_policy=None, optimizer_value=None,
+                 opt_num_epoches=1, value_opt_niter=1, **kwargs):
+        super().__init__(**kwargs)
+        self.tau = tau
+        self.optimizer_policy = optimizer_policy
+        self.optimizer_value = optimizer_value
+        self.opt_num_epoches = opt_num_epoches
+        self.value_opt_niter = value_opt_niter
