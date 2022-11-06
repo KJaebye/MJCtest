b0af10722bf955900f3951b5e431427366459c62
kangyao huang <kangyao.huang@outlook.com>
2022-11-06 21:50:20 +0800

    upload

--- structural_control/envs/centipede.py
+++ structural_control/envs/centipede.py
@@ -16,6 +16,8 @@
 from abc import ABC
 
 from lib.envs.mujoco_env import MujocoEnv, MujocoPhysics, MujocoTask
+from dm_control.rl.control import flatten_observation
+import dm_env
 
 
 class CentipedeEnv(MujocoEnv):
@@ -23,19 +25,35 @@ class CentipedeEnv(MujocoEnv):
         self.cfg = cfg
         self.mujoco_xml_string = None
         self.mujoco_xml_path = "./assets/robot_models/centipede/centipede_four.xml"
-        physics = CentipedePhysics.from_xml_path(self.mujoco_xml_path)
-        task = CentipedeTask()
-        super().__init__(physics, task)
+        self._physics = CentipedePhysics.from_xml_path(self.mujoco_xml_path)
+        self._task = CentipedeTask()
+        super().__init__(self._physics, self._task, flat_observation=False)
+        self._step_count = 0
 
     def reset(self):
-        pass
+        """ Starts a new episode and returns the first `TimeStep`. """
+        self._reset_next_step = False
+        self._step_count = 0
+
+        # initiate states/configuration of physics
+        with self._physics.reset_context():
+            self._task.initialize_episode(self._physics)
+
+        # get initial observation
+        observation = self._task.get_observation(self._physics)
+        if self._flat_observation:
+            observation = flatten_observation(observation)
+        return dm_env.TimeStep(
+            step_type=dm_env.StepType.FIRST,
+            reward=None,
+            discount=None,
+            observation=observation)
 
     def step(self, action):
         pass
 
     def render(self):
         pass
-
 
 
 class CentipedePhysics(MujocoPhysics):
